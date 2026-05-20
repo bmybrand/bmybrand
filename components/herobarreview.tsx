79ba3cd88151482021fdc169fa580b1daeee20e1
hassanMughis <mughishassan0@gmail.com>
2026-05-21 01:24:16 +0500

    Refactor code structure for improved readability and maintainability

--- components/herobarreview.tsx
+++ components/herobarreview.tsx
@@ -55,7 +55,7 @@ const herobarreview = () => {
           }`}
           style={{ transform: `translate3d(${-offset.x}px, ${-offset.y}px, 0)` }}
         >
-            <img src="/bmyb-global-technlogicalbear-01.webp" alt="About us" className="h-auto max-h-full w-full object-contain lg:pt-30 pt-10" />
+            <img src="/bmyb-review-hero-01.webp" alt="About us" className="h-auto max-h-full w-full object-contain lg:pt-30 pt-10" />
         </div>
         </div>
     </div> 
