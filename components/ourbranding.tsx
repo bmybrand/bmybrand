62ec72dbcf86178828437059fe8be40a3d01049e
hassanMughis <mughishassan0@gmail.com>
2026-06-04 23:08:55 +0500

    lighthouse fix

--- components/ourbranding.tsx
+++ components/ourbranding.tsx
@@ -150,7 +150,7 @@ const OurBranding: React.FC = () => {
                 style={positions[index]}
               >
                 <div className="flex items-start gap-4">
-                  <Image
+                  <img
                     src={card.icon}
                     alt=""
                     width={48}
@@ -183,7 +183,7 @@ const OurBranding: React.FC = () => {
               className="w-full lg:w-sm bg-[#191A35] border border-[#2A2B47] rounded-xl py-4 px-6 mx-auto"
             >
               <div className="flex items-start gap-4">
-                <Image
+                <img
                   src={card.icon}
                   alt=""
                   width={48}
